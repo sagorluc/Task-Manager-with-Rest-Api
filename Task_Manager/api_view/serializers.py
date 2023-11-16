09a467148d407a9331b087164765799feab0b5f6
sagorluc <mdsagorluc@gmail.com>
2023-11-16 17:29:39 +0600

    api update

--- Task_Manager/api_view/serializers.py
+++ Task_Manager/api_view/serializers.py
@@ -1,10 +1,23 @@
 from rest_framework import serializers
-from tasks.models import TaskModel
+from tasks.models import TaskModel, TaskImage
+from django.contrib.auth.models import User
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model  = User
+        fields = ['id']
+
+        
 
 class TaskSerializer(serializers.ModelSerializer):
+    user   = CustomUserSerializer(read_only= True) # hide user from input
+    images = serializers.ListField(child=serializers.ImageField(), write_only=True, required=False)
+    
     class Meta:
         model  = TaskModel
         fields = ['user', 'title', 'description', 
                   'due_date', 'priority', 'complete', 
-                  'created_at', 'updated_at'
+                  'created_at', 'updated_at', 'images'
                   ]
+        
--- Task_Manager/api_view/views.py
+++ Task_Manager/api_view/views.py
@@ -2,14 +2,32 @@ from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from tasks.models import TaskModel
 from api_view.serializers import TaskSerializer
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import authenticate
 # Create your views here.
 
 class TaskListCreateAPIView(ListCreateAPIView):
-    queryset         = TaskModel.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return TaskModel.objects.filter(user= self.request.user)
+    
+    def perform_create(self, serializer):
+        return serializer.save(user= self.request.user)
+    
+    # def get_serializer_class(self):
+    #     if self.request.method == 'POST':
+    #         return CreateTaskSerializer
+    #     else:
+    #         return TaskSerializer
+            
     
 
 class TaskRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    queryset         = TaskModel.objects.all()
+    # queryset         = TaskModel.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return TaskModel.objects.filter(user= self.request.user)
